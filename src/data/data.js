28fe8de95dd6d1b60df339ef99eac962ca410dfc
Samir Rai <samikhaling14@gmail.com>
2023-07-04 22:50:03 -0400

    initial commit

---
+++ src/data/data.js
@@ -0,0 +1,21 @@
+import Deskbook from '../assets/projects/deskbook.jpg'
+import Book from '../assets/projects/book.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:Deskbook,
+        github:"https://github.com/samikhaling/Desk-booking-system",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:Book,
+        github:"https://github.com/samikhaling/book-hooks",
+        live:"",
+    },
+ 
+
+]
