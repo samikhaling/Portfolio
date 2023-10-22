cbaa3e873649fc35f5abdad73087ace1bb8fe881
Samir Rai <samikhaling14@gmail.com>
2023-10-22 09:29:41 -0400

    project added

--- src/data/data.js
+++ src/data/data.js
@@ -3,38 +3,46 @@ import clothing from '../assets/projects/samclothing.jpg'
 import pizza from '../assets/projects/pizza.jpg'
 import jobpost from '../assets/projects/jobposting.jpg'
 import quizz from '../assets/projects/quizz.jpg'
+import lms from '../assets/projects/lms-learning.png'
 
 export const data=[
-    {
+     {
         id:1,
+        name:"Lms-Learning using Next.js",
+        image:lms,
+        github:"https://github.com/samikhaling/lms-tutorial",
+        live:"https://lms-tutorial-rho.vercel.app/",
+    },
+    {
+        id:2,
         name:"React quizz application",
         image:quizz,
         github:"https://github.com/samikhaling/react-quiz",
         live:"https://samir-react-quizz.netlify.app/",
     },
     {
-        id:2,
+        id:3,
         name:"Ecommerce for clothing",
         image:clothing,
         github:"https://github.com/samikhaling/samirclothing",
         live:"https://samir-clothing.netlify.app/",
     },
     {
-        id:3,
+        id:4,
         name:"Pizza order application",
         image:pizza,
         github:"https://github.com/samikhaling/pizza-delivery",
         live:"https://fast-pizzadelivery.netlify.app/",
     },
     {
-        id:4,
+        id:5,
         name:"Job Posting application",
         image:jobpost,
         github:"https://github.com/samikhaling/job-post",
         live:"https://job-post-76qs4xqtj-samikhaling.vercel.app/jobs",
     },
     {
-        id:5,
+        id:6,
         name:"Restaurent booking application",
         image:restaurent,
         github:"https://github.com/samikhaling/restaurent",
